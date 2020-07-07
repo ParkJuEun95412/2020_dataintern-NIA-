81f6abcaf538756363de7d04ed6bfcc127f6630b
ParkJuEun95412 <57995077+ParkJuEun95412@users.noreply.github.com>
2020-07-07 10:04:27 +0900

    Create R통계2(0707).R

---
+++ R통계2(0707).R
@@ -0,0 +1,24 @@
+#0707
+#데이터 준비
+four_sessions <- read.csv('c:/r/data/four_sessions.csv')
+
+#분산분석(ANOVA) 
+
+#일원 분산 분석 (독립변수가 하나일때)
+#출력방법만 다를 뿐 같은 결과물을 출력해낸다
+
+#aov() 함수 사용
+summary(aov(Time~Page,data=four_sessions))
+#oneway.test() 사용
+oneway.test(four_sessions$Time~four_sessions$Page,var=T) 
+
+#카이제곱검정
+#데이터 준비
+click_rate <- read.csv('c:/r/data/click_rates.csv')
+
+clicks <- matrix(click_rate$Rate, nrow=3,ncol=2,byrow=TRUE)
+#뉴스 헤드라인 별 클릭 수 데이터
+head(clicks)
+
+#chisq.test()
+chisq.test(clicks,simulate.p.value = TRUE) #귀무가설 채택 
